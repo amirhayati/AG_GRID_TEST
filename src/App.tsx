9def6babb588f72258aa35208795b5156cbdb0f0
Amir Hossein Hayati <amir_hossein_hayati@yahoo.com>
2024-10-02 14:26:33 +0330

    Show Visible Culomn

--- src/App.tsx
+++ src/App.tsx
@@ -27,26 +27,28 @@ const Home: React.FC = () => {
           if (jsonData.Entity && jsonData.Entity.columns) {
             console.log(jsonData.Entity.columns)
             // ------ Map columns to AG Grid format ---------
-            const columns = jsonData.Entity.columns.map((col: ColumnDataType) => ({
-              headerName: col.title || col.field,
-              field: col.field,
-              sortable: col.sortable || false,
-              align: col.align || "center",
-              lockPosition: col.align,
-              lockVisible: col.IsVisible,
-              minWidth: col.width,
-              cellClass: col.cellClass || '',
-              floatingFilter: true,
-              filter: 
-                col.ColumnType === 3 || col.ColumnType === 4 || ["3", "4"].includes(String(col.ColumnType)) 
-                  ? 'agTextColumnFilter' 
-                  : (Number(col.ColumnType) >= 5 && Number(col.ColumnType) <= 12) || ["5", "6", "7", "8", "9", "10", "11", "12"].includes(String(col.ColumnType)) 
-                  ? 'agNumberColumnFilter' 
-                  : col.ColumnType === 16 || col.ColumnType === 24 || ["16", "24"].includes(String(col.ColumnType))  
-                  ? 'agDateColumnFilter' 
-                  : col.ColumnType === 2 || ["2"].includes(String(col.ColumnType))
-                  ? 'agSetColumnFilter'
-                  : false,
+            const columns = jsonData.Entity.columns
+              .filter((col: ColumnDataType) => col.IsVisible)
+              .map((col: ColumnDataType) => ({
+                headerName: col.title || col.field,
+                field: col.field,
+                sortable: col.sortable || false,
+                align: col.align || "center",
+                lockPosition: col.align,
+                lockVisible: col.IsVisible,
+                minWidth: col.width,
+                cellClass: col.cellClass || '',
+                floatingFilter: true,
+                filter: 
+                  col.ColumnType === 3 || col.ColumnType === 4 || ["3", "4"].includes(String(col.ColumnType)) 
+                    ? 'agTextColumnFilter' 
+                    : (Number(col.ColumnType) >= 5 && Number(col.ColumnType) <= 12) || ["5", "6", "7", "8", "9", "10", "11", "12"].includes(String(col.ColumnType)) 
+                    ? 'agNumberColumnFilter' 
+                    : col.ColumnType === 16 || col.ColumnType === 24 || ["16", "24"].includes(String(col.ColumnType))  
+                    ? 'agDateColumnFilter' 
+                    : col.ColumnType === 2 || ["2"].includes(String(col.ColumnType))
+                    ? 'agSetColumnFilter'
+                    : false,
             }));
 
             console.log(columns)
